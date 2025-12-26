d33db6a3316c48caa05fdfe7f7db5da8832bb85e
hyojunez99 <hyojunez99@gmail.com>
2025-12-26 18:19:45 +0900

    2025.12.26-update05

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import MainPage from "./pages/MainPage";
 import CartPage from "./pages/CartPage";
 import CategoryPage from "./pages/CategoryPage";
+import DetailPage from "./pages/DetailPage";
+import { useState } from "react";
 import "./assets/scss/global.scss";
 
-import { useState } from "react";
-import DetailPage from "./pages/DetailPage";
 const App = () => {
-
-
-  //장바구니 아이템 임시입니다
   const [cartItems, setCartItems] = useState([
     { id: 1, title: "테스트 상품", price: 80000, quantity: 1, image: "shoes01-1.jpg" },
     { id: 2, title: "테스트 상품2", price: 129000, quantity: 2, image: "shoes01-1.jpg" },
   ]);
+
   const onUpdateQty = (id, delta) => {
-    setCartItems((prev) =>
-      prev.map((item) =>
-        item.id === id
-          ? { ...item, quantity: Math.max(1, item.quantity + delta) }
-          : item
+    setCartItems(prev =>
+      prev.map(item =>
+        item.id === id ? { ...item, quantity: Math.max(1, item.quantity + delta) } : item
       )
     );
   };
 
-  // ✅ 삭제
   const onDelete = (id) => {
-    setCartItems((prev) => prev.filter((item) => item.id !== id));
+    setCartItems(prev => prev.filter(item => item.id !== id));
   };
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<Layout />} >
           <Route path="/" element={<MainPage />} />
@@ -44,7 +38,7 @@ const App = () => {
           <Route path="/detail/:id" element={<DetailPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,15 +1,15 @@
-import DetailTop from '../components/detail/DetailTop';
-import DetailContent from '../components/detail/DetailContent';
-import DetailPick from '../components/detail/DetailPick';
-import DetailKeyword from '../components/detail/DetailKeyword';
+import DetailTop from "../components/detail/DetailTop";
+import DetailContent from "../components/detail/DetailContent";
+import DetailPick from "../components/detail/DetailPick";
+import DetailKeyword from "../components/detail/DetailKeyword";
 
 const DetailPage = () => {
   return (
-    <div className="detail-page">        
-        <DetailTop />
-        <DetailContent />
-        <DetailPick />
-        <DetailKeyword />
+    <div className="detail-page">
+      <DetailTop />
+      <DetailContent />
+      <DetailPick />
+      <DetailKeyword />
     </div>
   );
 };
