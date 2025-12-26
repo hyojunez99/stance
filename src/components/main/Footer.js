db7f25dd1502cfb4e9aaa1fd378384a6fe49f425
yezinnie14 <yezinnie14@gamil.com>
2025-12-26 15:26:00 +0900

    cart /footer update

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -1,82 +1,82 @@
-import React from "react";
+import  { useState } from "react";
 import "./Footer.scss";
-import { IoIosArrowDown } from "react-icons/io";
+import { FaInstagram, FaYoutube } from "react-icons/fa";
 
-const Footer = () => {
+function Footer() {
+  const [isDescOpen, setIsDescOpen] = useState(false);
   return (
-    <footer className="footer-container">
+    <footer className="footer">
       <div className="footer-inner">
-        {/* 1) 왼쪽: 회사 정보 */}
-        <section className="section-info">
-          <div className="logo-img">로고이미지</div>
-          <div className="brand">PACEY</div>
-
-          <div className="company-select">
-            PACEY <IoIosArrowDown className="arrow-icon" />
+        <div className="section brand">
+          <div className="logo-img">
+            <span className="logo-label">로고</span>
+            <div className="footer-logo">PACEY</div>
           </div>
+          <p className="tap"onClick={()=> setIsDescOpen(prev => !prev)}>PACEY{""}<span className="tap-arrow">{isDescOpen ? "∧" : "∨"}</span></p>
 
-          <p className="desc-txt">
-            PACEY은 통신판매중개자이며, 통신판매의 당사자가 아닙니다.
-            <br className="pc-only" />
+          <p className={`footer-txt ${isDescOpen ? "is-open" : ""}`}>
+            PEACY는 통신판매중개자이며, 통신판매의 당사자가 아닙니다. 
+            <br />
             입점 판매자의의 상품, 거래정보 및 거래에 대하여 책임을 지지 않습니다.
-            <br className="pc-only" />
-            단, PACEY이 판매자로 등록 판매한 상품은 판매자로서 책임을 부담합니다.
+            <br />
+            단, PEACY는 판매자로 등록 판매한 상품은 판매자로서 책임을 부담합니다.
           </p>
 
-          <nav className="nav-links policy-links">
-            <a href="#!">이용약관</a>
-            <a href="#!">개인정보처리방침</a>
-            <a href="#!">입점/제휴</a>
-          </nav>
-        </section>
-
-        {/* 2) 중앙: 고객 센터 */}
-        <section className="section-contact">
-          <div className="section-header">
-            <span className="title">
-              CUSTOMER<br />CENTER
-            </span>
-
-            <nav className="q-links">
-              <a href="#!">자주묻는 질문</a>
-              <a href="#!">1:1 문의</a>
-            </nav>
+          <div className="link">
+            <a href="#!" className="de-link">
+              이용약관
+            </a>
+            <a href="#!" className="de-link">
+              개인정보처리방침
+            </a>
+            <a href="#!" className="de-link">
+              입점/제휴
+            </a>
           </div>
+        </div>
 
+        <div className="section center">
+          <div className="center-title">CUSTOMER<br/>CENTER</div>
           <div className="phone">1255 - 1255</div>
+          <p className="time">
+            평일 10:00 - 17:00 (점심 12:00 - 13:00)
+          </p>
+          <div className="link small">
+            <div className="right-arr">
+            <a href="#!" className="de-link">
+              자주 묻는 질문
+            </a>
+            <a href="#!" className="de-link">
+              1:1 문의
+            </a>
+            </div>
+          </div>
+        </div>
 
-          <p className="desc-txt">평일 10:00 - 17:00 (점심 12:00-13:00)</p>
-        </section>
+        <div className="section right">
+            <div className="story-txt">PACEY <br/>STORY</div>
+            <div className="right-arr">
+            <a href="#!" className="de-link">
+              PACEY 스토리
+            </a>
+            </div>
 
-        {/* 3) 오른쪽: 스토리 & SNS */}
-        <section className="section-social">
-          <div className="story-area">
-            <div className="section-header">
-              <span className="title">
-                PACEY<br className="pc-only" />
-                STORY
-              </span>
-              <a href="#!" className="story-link">
-                PACEY 스토리
+          <div className="sm-block sns">
+            <div className="footer-title">SNS</div>
+
+            <div className="sns-icons ">
+              <a href="#!" aria-label="instagram" className="sns-icon">
+                <FaInstagram />
+              </a>
+              <a href="#!" aria-label="youtube" className="sns-icon">
+                <FaYoutube />
               </a>
             </div>
           </div>
-
-          <div className="sns-area">
-            <span className="title sns-title">SNS</span>
-            <div className="sns-icons">
-              <a href="#!" className="sns-icon">
-                <i class="fa-brands fa-instagram"></i>
-              </a>
-              <a href="#!" className="sns-icon">
-                <i class="fa-brands fa-youtube"></i>
-              </a>
-            </div>
-          </div>
-        </section>
+        </div>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
