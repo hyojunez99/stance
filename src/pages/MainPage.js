241c581516692eaf90fd67e56f112fb93e8bc049
yezinnie14 <yezinnie14@gamil.com>
2025-12-29 14:18:35 +0900

    img

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,8 +2,6 @@ import BannerSection from "../components/main/BannerSection";
 import BannerSection2 from "../components/main/BannerSection2";
 import BottomBlog from "../components/main/BottomBlog";
 import CategorySection from "../components/main/CategorySection";
-import Footer from "../components/main/Footer";
-// import Header from "../components/main/Header";
 import MidBlog from "../components/main/MidBlog";
 import ProductSection from "../components/main/ProductSection";
 import ProductSection2 from "../components/main/ProductSection2";
@@ -12,7 +10,6 @@ import TopBlog from "../components/main/TopBlog";
 const MainPage = () => {
   return (
     <div className="main-page">
-      {/* <Header/> */}
       <BannerSection />
       <CategorySection />
       <TopBlog />
